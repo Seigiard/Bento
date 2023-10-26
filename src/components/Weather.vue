8d219319b457561f45590697d8d77ddd3e62fa2c
Andrew Borisenko <seigiard@gmail.com>
2023-10-26 20:19:23 +0200

    Update weather icon

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,81 +1,121 @@
 <template>
   <div class="weather">
     <div class="weatherIcon">
-      <img :src="'assets/icons/' + iconType + '/' + weather.iconId + '.png'" alt="" />
+      <img :src="svgIcon" alt="" />
     </div>
     <div class="weatherValue">
-      <p>{{weather.temperature.value}} °{{weather.temperature.unit}}</p>
+      <p>{{ weather.temperature.value }} °{{ weather.temperature.unit }}</p>
     </div>
     <div class="weatherDescription">
-      <p>{{weather.description}}</p>
+      <p>{{ weather.description }}</p>
     </div>
   </div>
 </template>
 
 <script>
-  import CONFIG from '../export-config';
+import CONFIG from '../export-config';
 
-  const KELVIN = 273.15;
+const SvgIconsMap = {
+  unknown: 'not-available',
+  '01d': 'clear-day',
+  '01n': 'clear-night', // clear sky
+  '02d': 'partly-cloudy-day',
+  '02n': 'partly-cloudy-day', // few clouds
+  '03d': 'cloudy',
+  '03n': 'cloudy', // scattered clouds
+  '04d': 'overcast',
+  '04n': 'overcast', // broken clouds
+  '09d': 'rain',
+  '09n': 'rain', // shower rain
+  '10d': 'partly-cloudy-day-drizzle',
+  '10n': 'partly-cloudy-night-drizzle', // rain
+  '11d': 'thunderstorms-day',
+  '11n': 'thunderstorms-night', // thunderstorm
+  '13d': 'partly-cloudy-day-snow',
+  '13n': 'partly-cloudy-night-snow', // snow
+  '50d': 'fog-day',
+  '50n': 'fog-night', // mist
+};
 
-  export default {
-    data() {
-      return {
-        weather:{
-          temperature: {
-            unit: 'c',
-            value: '-',
-          },
-          description: '',
-          iconId: 'unknown',
+const KELVIN = 273.15;
+
+export default {
+  data() {
+    return {
+      weather: {
+        temperature: {
+          unit: 'c',
+          value: '-',
         },
-        iconType: CONFIG.weatherIcons,
-      };
-    },
-    created() {
-      setPosition().then(data => this.weather = data);
-    }
-  }
-
-  function setPosition() {
-    if (!CONFIG.trackLocation || !navigator.geolocation) {
-      if (CONFIG.trackLocation) {
-        console.error('Geolocation not available');
-      }
-      return getWeather(CONFIG.defaultLatitude, CONFIG.defaultLongitude);
-    }
-
-    return new Promise((resolve) => {
-      navigator.geolocation.getCurrentPosition(
-        pos => {
-          resolve(getWeather(pos.coords.latitude.toFixed(3), pos.coords.longitude.toFixed(3)));
-        },
-        err => {
-          console.error(err);
-          resolve(getWeather(CONFIG.defaultLatitude, CONFIG.defaultLongitude));
-        }
+        description: '',
+        iconId: 'unknown',
+      },
+      iconType: CONFIG.weatherIcons,
+    };
+  },
+  computed: {
+    svgIcon() {
+      return (
+        'assets/icons/' +
+        CONFIG.weatherIcons +
+        '/' +
+        SvgIconsMap[this.weather.iconId] +
+        '.svg'
       );
+    },
+  },
+  created() {
+    getWeather().then((data) => (this.weather = data));
+  },
+};
+
+function getWeather() {
+  if (!CONFIG.trackLocation || !navigator.geolocation) {
+    if (CONFIG.trackLocation) {
+      console.error('Geolocation not available');
+    }
+    return fetchWeatherData(CONFIG.defaultLatitude, CONFIG.defaultLongitude);
+  }
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        resolve(
+          fetchWeatherData(
+            pos.coords.latitude.toFixed(3),
+            pos.coords.longitude.toFixed(3)
+          )
+        );
+      },
+      (err) => {
+        console.error(err);
+        resolve(
+          fetchWeatherData(CONFIG.defaultLatitude, CONFIG.defaultLongitude)
+        );
+      }
+    );
+  });
+}
+
+function fetchWeatherData(latitude, longitude) {
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${CONFIG.language}&appid=${CONFIG.weatherKey}`;
+  return fetch(api)
+    .then(function (response) {
+      let data = response.json();
+      return data;
     })
-  }
+    .then(function (data) {
+      const weather = {};
+      let celsius = Math.floor(data.main.temp - KELVIN);
+      weather.temperature = {
+        unit: CONFIG.weatherUnit,
+      };
+      weather.temperature.value =
+        CONFIG.weatherUnit == 'C' ? celsius : (celsius * 9) / 5 + 32;
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
 
-  function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${CONFIG.language}&appid=${CONFIG.weatherKey}`;
-    return fetch(api)
-      .then(function (response) {
-        let data = response.json();
-        return data;
-      })
-      .then(function (data) {
-        const weather = {};
-        let celsius = Math.floor(data.main.temp - KELVIN);
-        weather.temperature = {
-          unit: CONFIG.weatherUnit,
-        };
-        weather.temperature.value = CONFIG.weatherUnit == 'C' ? celsius : (celsius * 9) / 5 + 32;
-        weather.description = data.weather[0].description;
-        weather.iconId = data.weather[0].icon;
-
-        return weather
-      })
-  }
-
+      return weather;
+    });
+}
 </script>
