88e24db33193806ac2828786c42d2ea7bac36de9
Borisenko Andrew <seigiard@gmail.com>
2024-04-02 22:00:33 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,7 +10,6 @@ const CONFIG = {
 
   // Weather
   weatherKey: '9e2df1fb216a5477f862b69f1732af0c', // Write here your API Key
-  weatherIcons: 'FillSvgColorStatic', // 'FillSvgColor', 'FillSvgColorStatic', 'LineSvgColor', 'LineSvgColorStatic', 'SvgMonochrome'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
