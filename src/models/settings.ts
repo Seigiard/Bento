174bcdefe2a628729b4d9804cdc346f85f9b66eb
Andrew Borisenko <seigiard@gmail.com>
2024-05-23 07:35:22 +0200

    Update theme

--- src/models/settings.ts
+++ src/models/settings.ts
@@ -1,11 +1,57 @@
-import { SettingsValue } from "../nanostores/settings";
+export type SettingsValue = {
+  theme: 'dark' | 'light' | 'system'
+  name: string
+  greetingMorning: string
+  greetingAfternoon: string
+  greetingEvening: string
+  greetingNight: string
+  weatherKey: string
+  weatherUnit: 'F' | 'C'
+  weatherDefaultLatitude: string
+  weatherDefaultLongitude: string
+  weatherLanguage: string
+  raindropApiKey: string
+  raindropCollection: string
+  ouraApiKey: string
+}
 
+export const themeList: SettingsValue['theme'][] = ['light', 'dark', 'system'];
+
+export const defaultSettings: SettingsValue = {
+  theme: 'system',
+
+  name: '',
+
+  // Greetings
+  greetingMorning: 'Good morning',
+  greetingAfternoon: 'Good afternoon',
+  greetingEvening: 'Good evening',
+  greetingNight: 'Go to Sleep',
+
+  // Weather
+  weatherKey: '9e2df1fb216a5477f862b69f1732af0c', // Write here your API Key
+  weatherUnit: 'C', // 'F', 'C'
+  weatherDefaultLatitude: '48.160570069000705',
+  weatherDefaultLongitude: '17.149771267456032',
+  weatherLanguage: 'en', // More languages in https://openweathermap.org/current#multi
+
+  // Raindrop.io
+  raindropApiKey: '',
+  raindropCollection: '',
+
+  // Oura
+  ouraApiKey: '',
+}
 
 export const SettingsFormFields: Partial<Record<keyof SettingsValue, {
   title: string;
   description?: string;
   values?: string[];
 }>> = {
+  theme: {
+    title: 'Theme',
+    values: themeList
+  },
   name: {
     title: 'Name',
     description: 'Your name or nickname'
