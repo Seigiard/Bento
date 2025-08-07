b86225945738c0736e42218713a0559ac9b598c2
Borisenko Andrew <seigiard@gmail.com>
2025-08-07 09:45:06 +0200

    Add Raindrop API fetcher with IndexedDB cache

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,60 @@ import { Settings } from './components/Settings'
 import { Category } from './components/Category'
 import { useStore } from '@nanostores/preact'
 import { $collections } from './nanostores/collections';
+import { $fetcherReady } from './nanoquery/raindrop-fetcher';
 
 export function App() {
+  return (
+    <main class='p-6 w-dvw h-dvh gap-8 overflow-x-scroll [column-count:auto] [column-width:clamp(20rem,25vw,23rem)] [column-fill:auto]'>
+      <AppLoader />
+      <Settings />
+    </main>
+  )
+}
+
+function AppLoader() {
+  const fetcherReady = useStore($fetcherReady)
   const { raindropApiKey } = useStore($settings)
+
+  if(!fetcherReady) {
+    return <div>Loading...</div>
+  }
+
+  if(!raindropApiKey) {
+    return (
+        <div class="alert alert-warning">
+          <span>Please set your Raindrop API key in settings</span>
+        </div>
+    )
+  }
+
+  return <TheApp />
+}
+
+function TheApp() {
   const { loading, data: collections, error } = useStore($collections)
 
   return (
-    <div class="app">
-      <Settings />
-      <main class='p-6 w-dvw h-dvh gap-8 overflow-x-scroll [column-count:auto] [column-width:clamp(20rem,25vw,23rem)] [column-fill:auto]'>
-        {!raindropApiKey && (
-          <div class="alert alert-warning">
-            <span>Please set your Raindrop API key in settings</span>
-          </div>
-        )}
+    <>
+      {loading && (
+        <div class="loading loading-spinner loading-lg"></div>
+      )}
 
-        {loading && (
-          <div class="loading loading-spinner loading-lg"></div>
-        )}
+      {error && (
+        <div class="alert alert-error">
+          <span>Error loading categories: {error.message}</span>
+          <button
+            class="btn btn-ghost btn-sm"
+            onClick={() => window.location.reload()}
+          >
+            Retry
+          </button>
+        </div>
+      )}
 
-        {error && (
-          <div class="alert alert-error">
-            <span>Error loading categories: {error.message}</span>
-            <button
-              class="btn btn-ghost btn-sm"
-              onClick={() => window.location.reload()}
-            >
-              Retry
-            </button>
-          </div>
-        )}
-
-        {!loading && !error && collections?.map((collection) => (
-          <Category key={collection._id} collection={collection} />
-        ))}
-      </main>
-    </div>
+      {!loading && !error && collections?.map((collection) => (
+        <Category key={collection._id} collection={collection} />
+      ))}
+    </>
   )
 }
--- src/nanoquery/raindrop-fetcher.ts
+++ src/nanoquery/raindrop-fetcher.ts
@@ -1,8 +1,38 @@
 import { nanoquery } from '@nanostores/query';
+import { atom, onStart } from 'nanostores';
 import { $settings } from '../nanostores/settings';
 import { CollectionType, RaindropItemType, safeParseCollectionResponse, safeParseRaindropResponse, safeParseUserResponse, UserType } from '../schemas/raindrop-schemas';
+import { get, set, keys } from '@isomorphic-git/idb-keyval';
+
+const cacheKey = 'raindropFetcher-';
+
+class Cache extends Map {
+  set(key: string, value: any, persist = true) {
+    const res = super.set(key, value);
+    persist && set(`${cacheKey}${key}`, value);
+    return res;
+  }
+}
+const cache = new Cache();
+
+async function initCache() {
+  const keys_ = await keys()
+  return Promise.all(keys_.map(async (key) => {
+    if (typeof key === 'string' && key.startsWith(cacheKey)) {
+      const value = await get(key)
+      cache.set(key.replace(cacheKey, ''), value, false);
+    }
+  }))
+}
+
+export const $fetcherReady = atom(false);
+onStart($fetcherReady, () => {
+  initCache().then(() => $fetcherReady.set(true));
+});
 
 const [createRaindropApiFetcherStore] = nanoquery({
+  cache,
+  dedupeTime: 1000 * 60 * 60, // 1 hour
   fetcher: async (...keys) => {
     try {
       const response = await fetch(`https://api.raindrop.io/rest/v1${keys.join('')}`, {
