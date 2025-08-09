40b98f197563843a1a4583d48259f5cb8e91adc3
Borisenko Andrew <seigiard@gmail.com>
2025-08-09 13:38:54 +0200

    Add service worker registration back to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { render } from 'preact'
 import { App } from './App'
 
-// navigator.serviceWorker.register(
-//   new URL('./service-worker.js', import.meta.url),
-//   {type: 'module'}
-// );
-//
+navigator.serviceWorker.register(
+  new URL('./service-worker.js', import.meta.url),
+  {type: 'module'}
+);
 
 const root = document.getElementById('app')
 if (root) {
